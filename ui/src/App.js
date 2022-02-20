9ece8cabc465beada28b08b61ee5434633dd162e
Ryan Ross <ryanross@Bretts-MBP.attlocal.net>
2022-02-20 13:00:55 -0600

    reworking ui

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+
+const baseURL = "http://localhost:3000/posts/1";
 
 function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{post.content}</h1>
     </div>
   );
 }
